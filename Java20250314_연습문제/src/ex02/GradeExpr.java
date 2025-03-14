3afe68af2c7e81b12a92595c7b4acc3ef44c2966
sinaboro <rlaeocjf@gmail.com>
2025-03-14 11:10:06 +0900

    .......

---
+++ Java20250314_연습문제/src/ex02/GradeExpr.java
@@ -0,0 +1,45 @@
+package ex02;
+
+public class GradeExpr {
+	private int[] jumsu;
+//	private int sum=0;
+
+	public GradeExpr() {
+		
+	}
+
+	public GradeExpr(int[] jumsu) {
+		this.jumsu = jumsu;
+	}
+	
+	int getTotal() {
+		int sum=0;
+		for(int i=0; i<jumsu.length; i++)
+			sum += jumsu[i];
+		
+		return sum;
+	}
+	
+	double getAverage() {
+		return (double)getTotal()/jumsu.length;
+	}
+	
+	int getGoodScore() {
+		int max= jumsu[0];
+		for(int i=0; i<jumsu.length; i++) {
+			if(max < jumsu[i])
+				max = jumsu[i];
+		}
+			
+		return max;
+	}
+	
+	int getBadScore() {
+		int min= jumsu[0];
+		for(int i=0; i<jumsu.length; i++) {
+			if(min > jumsu[i])
+				min = jumsu[i];
+		}
+		return min;
+	}
+}
---
+++ Java20250314_연습문제/src/ex02/GradeTest.java
@@ -0,0 +1,46 @@
+package ex02;
+
+import java.util.Scanner;
+
+/*
+ - 처리하려는 데이터의 갯수를 표준 입력으로 받는다.(Scanner 사용)
+- 입력된 값을 크기로 갖는 int 타입 배열을 생성한다.
+- 표준입력으로 데이터를 받아서 첫 번째 원소부터 마지막 원소까지 저장한다.
+- 저장된 원소 값들을 하나의 행에, 기호로 분리하여 출력한다.
+- 만들어진 배열을 전달하면서 GradeExpr 객체를 생성한 후에 총점과 평균을 구하는
+메서드를 호출하여 수행 결과를 다음과 같은 형식으로 화면에 표준 출력한다.
+총점 : ...
+평균 : ...
+최고 점수 : ...
+최저 점수 : ... 
+ */
+public class GradeTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("데이타 갯수>>");
+		int count = sc.nextInt();
+		
+		int[] nArr = new int[count];
+		
+		for(int i=0; i<nArr.length; i++) {
+			System.out.println("정수 입력>>");
+			nArr[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<nArr.length; i++) {
+			System.out.print(nArr[i] + ", ");
+		}
+		System.out.println();
+		
+		GradeExpr expr = new GradeExpr(nArr);
+		
+		System.out.println("총점 : " + expr.getTotal());
+//		System.out.println("평균점 : " + expr.getAverage());
+		System.out.printf("평균점 : %.2f\n", expr.getAverage());
+		System.out.println("최고 : " + expr.getGoodScore());
+		System.out.println("최저 : " + expr.getBadScore());
+	}
+
+}
